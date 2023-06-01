0faec6f59702a9b7738269019531c78f533491d6
benavio <dt76tank@gmail.com>
2023-06-01 22:57:55 +0300

    add CRUD struct

---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+type error struct {
+	Error string `json:"error"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jery", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbums album
+	if err := c.BindJSON(&newAlbums); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, error{"bad_request"})
+		return
+	}
+	albums = append(albums, newAlbums)
+	c.IndentedJSON(http.StatusCreated, newAlbums)
+}
+
+func updateAlbumsById(c *gin.Context) {
+	id := c.Param("id")
+	for i, a := range albums {
+		if a.ID == id {
+			c.BindJSON(&a)
+			albums[i] = a
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, error{"not found"})
+
+}
+
+func getAlbumById(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusCreated, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusBadRequest, error{"not found"})
+}
+
+func deleteAlbumById(c *gin.Context) {
+	id := c.Param("id")
+	for i, a := range albums {
+		if a.ID == id {
+			albums = append(albums[:i], albums[i+1:]...)
+			c.IndentedJSON(http.StatusNoContent, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusBadRequest, error{"not found"})
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumById)
+	router.DELETE("/albums/:id", deleteAlbumById)
+	router.PUT("/albums/:id", updateAlbumsById)
+	router.POST("/albums", postAlbums)
+
+	router.Run("localhost:8080")
+}
